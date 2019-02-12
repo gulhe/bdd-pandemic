9edd9fcef8f66b79488bf2c2e66f410acb6eba88
Guillaume Lhermitte <lhermitteg@gmail.com>
2019-02-12 14:19:45 +0100

    mvnize

---
+++ src/test/java/org/gulhe/pandemic/steps/MyStepdefs.java
@@ -0,0 +1,10 @@
+package org.gulhe.pandemic.steps;
+
+import cucumber.api.java.en.*;
+
+public class MyStepdefs {
+    @Given("^the occidental sub-network$")
+    public void theOccidentalSubNetwork() {
+
+    }
+}
---
+++ src/test/java/org/gulhe/pandemic/steps/Runner.java
@@ -0,0 +1,11 @@
+package org.gulhe.pandemic.steps;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty"})
+public class Runner {
+}
